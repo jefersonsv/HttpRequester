42f0177bae22633abf2438ef4d9240b007ea79f7
Jeferson Tenorio <jefersonsv@gmail.com>
2019-02-22 10:21:39 -0300

    initial commit

---
+++ sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using HttpRequester;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HttpCached
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CachedRequester cached = new CachedRequester(new Requester(EnumHttpProvider.HttpClient), duration: new TimeSpan(1, 0, 0));
+            var task = await cached.GetContentAsync("https://www.google.com", "");
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
